2b6163020587231a030b39091a3b9e01e90d55c9
Anastasia <anromanova.design@gmail.com>
2022-05-17 21:55:33 +0200

    about page bg

--- src/components/About/Index.jsx
+++ src/components/About/Index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Index.css";
+import theme from "../../images/theme.svg";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <body className="about">
+      <img src={theme} className="bg-image" alt="theme" />
+    </body>
+  );
 }
 
 export default About;
