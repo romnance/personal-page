37bd212f5e6a9f42eb1418620f72541a92728ca4
Anastasia <anromanova.design@gmail.com>
2022-05-26 11:22:45 +0200

    pdf viewer for resume

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Ukraine from "./components/Ukraine/Ukraine";
 import About from "./components/About/Index";
-import { Document, Page } from "react-pdf";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+import BackButton from "./components/Buttons/BlueButton";
 import CV from "./images/Anastasiia_Romanova_CVf.pdf";
-import base64String from "./Anastasiia_Romanova_CVf.txt";
 
 function App() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
     <Routes>
       <Route path="/" element={<Ukraine />} />
@@ -14,13 +22,18 @@ function App() {
       <Route
         path="/resume.pdf"
         element={
-          <Document file={`CV;base64,${base64String}`}>
-            <Page pageNumber={1} />
-          </Document>
+          <div className="resume">
+            <a href="/about">
+              <BackButton text="Back" />
+            </a>
+            <Document file={CV} onLoadSuccess={onDocumentLoadSuccess}>
+              <Page size="A4" pageNumber={pageNumber} />
+            </Document>
+          </div>
         }
       />
     </Routes>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  root
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
