be3f02f6639b8c7b1fc7944d9b3340b93247f4fc
Anastasia <anromanova.design@gmail.com>
2022-05-26 10:12:02 +0200

    index js update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ ReactDOM.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
